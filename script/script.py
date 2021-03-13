cd1c09cb0696cb98ca9a6842ca577332f7925991
Luís F. Miki <69601772+luisfmiki@users.noreply.github.com>
2021-03-13 17:27:22 -0300

    Update script.py

--- script/script.py
+++ script/script.py
@@ -2,11 +2,13 @@ import requests
 import re
 import glob
 
+# cria uma lista com os nomes dos arquivos, no caso são .txt
 myFiles = glob.glob('*.txt')
 for file in myFiles:
     f = open(file, 'r')
     payload = {"action":"ajax_hash", "text":"%s" % f.read(), "algo":"md2"}
     r = requests.post("https://www.tools4noobs.com/", data=payload)
+    # captura o valor de hash dentro do response do site
     m = re.search('</b> (.+?)</div>', r.text)
     if m:
         found = m.group(1)
@@ -15,3 +17,4 @@ for file in myFiles:
     hashes = h.read().split('\n')
     if found not in hashes:
         print("Alterado --> ", file)
+    h.close()
