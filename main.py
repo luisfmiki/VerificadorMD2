52fd1fb35581bbc19c708687c6e2a4fcc19ffd97
Luís F. Miki <69601772+luisfmiki@users.noreply.github.com>
2021-04-24 14:25:45 -0300

    Update main.py

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import requests
 import re
 import sys
 import os
-
+ 
 try:
     hashes = sys.argv[1]
     pastaDosArquivos = sys.argv[2]
@@ -12,9 +12,15 @@ try:
                 CamAbsoluto = os.getcwd() + "\\" + pastaDosArquivos + "\\"
             else:
                 CamAbsoluto = os.getcwd() + "\\" + pastaDosArquivos
-
+ 
             myFiles = [file for file in os.listdir(CamAbsoluto) if re.search('\.txt', file)]
-            
+ 
+            tabela_hash = {}
+            with open(hashes) as h:
+                for linha in h.readlines():
+                    arq, hsh = linha.rstrip('\n').split('\t')
+                    tabela_hash[arq] = hsh
+ 
             for file in myFiles:
                 with open(CamAbsoluto + file, 'r') as f:
                     text = list(f.read())
@@ -25,17 +31,16 @@ try:
                 m = re.search('</b> (.+?)</div>', r.text)
                 if m:
                     found = m.group(1)
-                with open(hashes, 'r') as h:
-                    hsh = h.read().split('\n')
-                    if found not in hsh:
-                        print("Alterado --> ", file)
+                if tabela_hash[file] != found:
+                    print("Alterado --> ", file)
         except IOError:
             print("Arquivo não acessivel")
     else:
-        print("Bad Input: o primeiro argumento deve ser um arquivo .txt e o segundo um diretório.")        
+        print("Bad Input: o primeiro argumento deve ser um arquivo hash e o segundo um diretório.")        
 except:
-    print("""Uso: python script.py <hash> <pasta>
-
-    Onde: <hash> - um arquivo .txt contendo os valores de hash separados por quebra de linha
-
+    print("""Uso: python main.py <hash> <pasta>
+ 
+    Onde: <hash> - um arquivo hash contendo os valores de hash separados por quebra de linha
+ 
           <pasta> - pasta/diretório contendo os arquivos a serem verificados a integridade, também em .txt\n""")
+ 
